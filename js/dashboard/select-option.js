198b424fc3e438a7bbb656b7a6362f858f240da8
Joaquin18T <torrescondo@933958993>
2024-10-13 22:26:54 -0500

    arreglo en actualizar activo

--- js/dashboard/select-option.js
+++ js/dashboard/select-option.js
@@ -1,27 +1,26 @@
 document.addEventListener("DOMContentLoaded",()=>{
-  const sidebarList = document.getElementById('options-sidebar');
-
-  sidebarList.addEventListener("click",(e)=>{
-    if (e.target && e.target.matches("a.nav-link")) {
-      const aElement = document.querySelectorAll(".nav-link");
-      aElement.forEach(x=>{
-        x.addEventListener("click",(e)=>{
-          e.preventDefault();
-          const hrefValue = enlace.href;
-          window.location.href= hrefValue;
+  if(document.getElementById('options-sidebar')){
+    const sidebarList = document.getElementById('options-sidebar');
+  
+    sidebarList.addEventListener("click",(e)=>{
+      if (e.target && e.target.matches("a.nav-link")) {
+        const aElement = document.querySelectorAll(".nav-link");
+        aElement.forEach(x=>{
+          x.addEventListener("click",(e)=>{
+            e.preventDefault();
+            const hrefValue = enlace.href;
+            window.location.href= hrefValue;
+          });
         });
-      });
+  
+        
+        let items = sidebarList.querySelectorAll('li');
+        items.forEach(item => item.classList.remove('active'));
+        
+        // Añadir la clase active al li seleccionado
+        e.target.parentElement.classList.add('active');
+      }
+    });
+  }
 
-      
-      let items = sidebarList.querySelectorAll('li');
-      items.forEach(item => item.classList.remove('active'));
-      
-      // Añadir la clase active al li seleccionado
-      e.target.parentElement.classList.add('active');
-    }
-  });
-
-  (async()=>{
-    
-  });
 });
--- views/index.php
+++ views/index.php
@@ -4,8 +4,7 @@ require_once 'header.php';
 ?>
 <div class="">
 
-  <a href="<?=$host?>/controllers/usuarios.controller.php?operation=destroy">aaaa</a>
-
+  <a href="<?=$host?>/controllers/usuarios.controller.php?operation=destroy">cerrar sesion</a>
 
 </div>
 <?php require_once 'footer.php' ?>
